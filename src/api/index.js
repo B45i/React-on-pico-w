a7cd0061f91c9d513f8c52c597ce6159404db6be
b45i <amalshajan2011@gmail.com>
2022-10-22 07:23:48 +0530

    update api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,24 @@
+const state = {
+    BTN_2: { name: 'TV', state: false },
+    BTN_3: { name: 'Bedroom Light', state: false },
+    BTN_4: { name: 'Water Heater', state: false },
+    BTN_1: { name: 'Washing Machine', state: false },
+};
+
 export const getButtonsAsync = async () => {
-    const response = await fetch(`api/buttonState`);
+    const response = await fetch(`api/get_buttons`);
+    const value = await response.json();
+    return value;
+};
+
+export const updateButtonAsync = async (id, state) => {
+    const response = await fetch(`api/update_button/${id}`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({ state }),
+    });
     const value = await response.json();
     return value;
 };
